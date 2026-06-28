7d8860d36a4795c7ac44f060f46b21ee85ca075f
Pablo <Pablo.Paniagua.J@gmail.com>
2026-06-28 10:54:47 -0400

    Fixed the issue with setting the PB5 bit. Previously it was writing to all the PB pins. Fixed the open-drain setting as well. Cleaned up the file. Updated the IDE so it replaces tabs with 4 spaces. It was getting annoying to view in the diffs.

--- main.s
+++ main.s
@@ -1,19 +1,16 @@
-	;INCLUDE C:\ti\TivaWare_C_Series-2.1.4.178\examples\boards\my_board\startup_TM4C123.s
-
-	AREA |.text|, CODE, READONLY
-	;THUMB
-	EXPORT __main
-	ENTRY
+    AREA |.text|, CODE, READONLY
+    EXPORT __main
+    ENTRY
 	
 __main
 
-	;defining addresses here for practice
+    ;defining addresses here for practice
 
-	;general base addresses
+    ;general base addresses
 SYS_CONTROL	EQU 0x400FE000
 AHB_PORTB	EQU 0x40059000
 	
-	;offsets
+    ;offsets
 GPIOHBCTL 	EQU 0x06C
 RCGCGPIO	EQU	0x608
 GPIODIR		EQU	0x400
@@ -27,69 +24,62 @@ GPIODEN		EQU	0x51C
 GPIODATA	EQU 0x00
 GPIODATAPB	EQU	0x3FC; 0x3FC is all bits in data register
 GPIODATAPB5	EQU 0x80 ; 0010 0000 dd (d = don't care)
-					 ;   ^-- pin5 (PB5)
+                     ;   ^-- pin5 (PB5)
 
 	
 	
-	;Note that GPIO can only be accessed through the AHB aperture
-	;select AHB bus. GPIOHBCTL
-	LDR r0, =SYS_CONTROL
-	LDR r1,[r0, #GPIOHBCTL]
-	ORR r1, r1, #(1<<1) ;Enable port B AHB instead.
-	;BFC r1,#0,#6 ;use APB when 0
-	;AND r1, r1, 0x0000.0000 ;use APB when 0
-	STR r1,[r0, #GPIOHBCTL] 
+    ;Note that GPIO can only be accessed through the AHB aperture
+    ;select AHB bus. GPIOHBCTL
+    LDR r0, =SYS_CONTROL
+    LDR r1,[r0, #GPIOHBCTL]
+    ORR r1, r1, #(1<<1) ;Enable port B AHB instead.
+    STR r1,[r0, #GPIOHBCTL] 
 	
-	;Enable clock to GPIO port B. RCGCGPIO
-	LDR r0, =SYS_CONTROL 
-	LDR r1,[r0,#RCGCGPIO]
-	ORR r1, r1, #(1<<1) ;enable port B clock(bit 5)
-	STR r1,[r0,#RCGCGPIO]
+    ;Enable clock to GPIO port B. RCGCGPIO
+    LDR r0, =SYS_CONTROL 
+    LDR r1,[r0,#RCGCGPIO]
+    ORR r1, r1, #(1<<1) ;enable port B clock(bit 5)
+    STR r1,[r0,#RCGCGPIO]
 	
-	;set GPIO Port B pin 5 to output. GPIODIR
-	LDR r0, =AHB_PORTB
-	LDR r1,[r0,#GPIODIR]
-	ORR r1, r1, #(1<<5);pin5
-	STR r1,[r0,#GPIODIR]
+    ;set GPIO Port B pin 5 to output. GPIODIR
+    LDR r0, =AHB_PORTB
+    LDR r1,[r0,#GPIODIR]
+    ORR r1, r1, #(1<<5);pin5
+    STR r1,[r0,#GPIODIR]
 	
-	;Set GPIO mode to I/O (not alternate function). GPIOAFSEL
-	LDR r0, =AHB_PORTB
-	LDR r1,[r0,#GPIOAFSEL]
-	BFC r1,#0,#8 ;clears fields. 0 = GPIO
-	;AND r1, r1, 0x0000.0000 ;0 = GPIO
-	STR r1,[r0,#GPIOAFSEL]
+    ;Set GPIO mode to I/O (not alternate function). GPIOAFSEL
+    LDR r0, =AHB_PORTB
+    LDR r1,[r0,#GPIOAFSEL]
+    BFC r1,#0,#8 ;clears fields. 0 = GPIO
+    STR r1,[r0,#GPIOAFSEL]
 	
-	;Set drive strength to 8mA. GPIODR8R
-	LDR r0, =AHB_PORTB
-	LDR r1,[r0,#GPIODR8R]
-	ORR r1, r1, #(1<<5);pin5
-	STR r1,[r0,#GPIODR8R]
+    ;Set drive strength to 8mA. GPIODR8R
+    LDR r0, =AHB_PORTB
+    LDR r1,[r0,#GPIODR8R]
+    ORR r1, r1, #(1<<5);pin5
+    STR r1,[r0,#GPIODR8R]
 	
-	;Set tooutput. No pull up. Must be push/pull or open drain
-	;set to pull up. GPIOPUR
-	LDR r0, =AHB_PORTB
-	LDR r1,[r0,#GPIOODR]
-	ORR r1, r1, #(1<<5) ;pin5
-	STR r1,[r0,#GPIOODR]
+    ; Must be push/pull or open drain
+    ; set to open drain. Default (0) is push-pull. GPIOODR
+    LDR r0, =AHB_PORTB
+    LDR r1,[r0,#GPIOODR]
+    ORR r1, r1, #(1<<5) ;pin5
+    STR r1,[r0,#GPIOODR]
 
-	;enable digital output. GPIODEN
-	LDR r0, =AHB_PORTB
-	LDR r1,[r0,#GPIODEN]
-	ORR r1,r1, #(1<<5);pin 1 = digital output enable
-	STR r1,[r0,#GPIODEN]
+    ;enable digital output. GPIODEN
+    LDR r0, =AHB_PORTB
+    LDR r1,[r0,#GPIODEN]
+    ORR r1,r1, #(1<<5);pin 1 = digital output enable
+    STR r1,[r0,#GPIODEN]
 	
-	;power pin PB5 to light up external LED
-	;Write "high" to data register. GPIODATA
-	LDR r0, =AHB_PORTB
-	MOV r1, #(1<<5) ; pin5
-	STR r1,[r0,#GPIODATAPB5]
-	;LDR r1,[r0,#GPIODATAPB]
-	;MOV r1, #0xF0
-	;STR r1,[r0,#GPIODATAPB]
-	;LDR r1,[r0,#GPIODATAPB]
+    ;power pin PB5 to light up external LED
+    ;Write "low" to data register for gnd. GPIODATA
+    LDR r0, =AHB_PORTB
+    MOV r1, #(0<<5) ; pin5
+    STR r1,[r0,#GPIODATAPB5]
 	
 	
 ; Infinite loop. Equivalent to while();. Prevents program from stopping immediately
 stop B stop	
 	
-	END
+    END
